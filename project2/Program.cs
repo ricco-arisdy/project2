fb06beea6c93ade0171b1d5fc5b5482b83c12527
riccoarisdy <riccoarisdy@gmail.com>
2022-10-17 20:42:02 +0700

    membuat elemen equal pivot

--- project2/Program.cs
+++ project2/Program.cs
@@ -64,4 +64,19 @@ namespace QuickSort
 
             while (i <= j)
             {
+                //search for an element greater than pivot
+                while ((arr[i]) <= pivot && (i <= high))
+                {
+                    i++;
+                    cmp_count++;
+                }
+                cmp_count++;
+
+                //search for an element less than or equal to pivot
+                while ((arr[j]) > pivot && (j >= low))
+                {
+                    j--;
+                    cmp_count++;
+                }
+                cmp_count++;
 

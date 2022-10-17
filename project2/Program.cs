e705a5daea1ee7dd6087d77620e55bad31d0ad73
riccoarisdy <riccoarisdy@gmail.com>
2022-10-17 20:35:56 +0700

    membuat array elements

--- project2/Program.cs
+++ project2/Program.cs
@@ -31,5 +31,11 @@ namespace QuickSort
             Console.WriteLine("Enter Array Elements");
             Console.WriteLine("\n====================");
 
-            
+            //get array elements
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
 

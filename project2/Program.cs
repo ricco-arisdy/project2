203218cc00223ba658988ba6d533083f9a2b0433
riccoarisdy <riccoarisdy@gmail.com>
2022-10-17 20:15:29 +0700

    Add project files.

---
+++ project2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters;
+using System.Security.Cryptography;
+using System.Xml;
+
+namespace QuickSort
+{
+    class Program
+    {
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0;
+        private int mov_count = 0;
+
+        //Number of elements in array
+        private int n;
+
+        void read()
+        {

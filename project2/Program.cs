12a7c18c6ffa64675d166c927ce3f7533b8362df
riccoarisdy <riccoarisdy@gmail.com>
2022-10-17 20:32:28 +0700

    membuat while  if dan else

--- project2/Program.cs
+++ project2/Program.cs
@@ -17,4 +17,14 @@ namespace QuickSort
 
         void read()
         {
+            while (true)
+            {
+                Console.WriteLine("Enter the number of elements in the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\n Array can have maximum 20 elements \n");
+
 
